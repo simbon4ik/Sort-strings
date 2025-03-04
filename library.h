f97493be24205e01eb1c94cf8c55b4bcf685e341
simbon4ik <karpov.ivan123@mail.ru>
2025-03-04 14:02:09 +0300

    add header guards in library.h

--- library.h
+++ library.h
@@ -1,5 +1,9 @@
+#ifndef LIBRARY
+#define LIBRARY
 
 int outfile(char **ptr, int len);
 int inputfile(char ***ptr, int *len);
 int strings_comp(const void *a, const void *b);     //Компаратор для сравнения строк 
 int words_comp(const void *a, const void *b);       //Компаратор для сравнения символов
+
+#endif
